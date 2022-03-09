dc73419f31c4b39b029bfcf4551444d381d78573
안유찬 <anyc123@hanmail.net>
2022-03-10 00:39:16 +0900

    update

--- GNBServer/main.cpp
+++ GNBServer/main.cpp
@@ -3,7 +3,7 @@
 #include "yc_worker.hpp"
 #include "packet_data.hpp"
 #include "yc_time.hpp"
-
+#include <map>
 auto w = yc_net::create_worker();
 
 
@@ -28,40 +28,40 @@ int main()
 {
     packet_data_load();
 
+    std::map<yc::socket_t, std::string> users;
+
     server_setting = server_setting_t{
         .io_thread_number = 2,
         .worker_thread_number = 2,
-        .port = 2738
+        .port = 6666
     };
 
-    yc_net::connect_callback = [](yc::socket_t socket) {
+    yc_net::connect_callback = [&](yc::socket_t socket) {
         printf("connect! [SOCKET : %lld, ADDR : %s]\n", socket, yc_net::get_clnt_addrs(socket).c_str());
+        users[socket] = yc_net::get_clnt_addrs(socket).c_str();
     };
 
-    yc_net::disconnect_callback = [](yc::socket_t socket) {
+    yc_net::disconnect_callback = [&](yc::socket_t socket) {
         printf("disconnect! [SOCKET : %lld, ADDR : %s]\n", socket, yc_net::get_clnt_addrs(socket).c_str());
+        users.erase(socket);
     };
-
-
     bool stop = false;
     int a = 0;
     strand_run(stop);
-
-    yc_net::bind_ev<p_test_packet_t>([](p_test_packet_t* packet, yc::socket_t socket) {
-        printf("SOCKET : %lld - %d\n", socket, packet->number);
-
-        auto p = p_packet01_t{
-            999
-        };
-        auto p2 = p_packet02_t{
-            555
-        };
-
-        yc_net::send(&p, socket);
-        yc_net::send(&p2, socket);
+    yc_net::bind_ev<p_chat_message_t>([&](p_chat_message_t* chat, yc::socket_t socket) {
+        wprintf(std::format(L"SOCKET : {} - {}\n", socket, chat->msg).c_str());
+        p_chat_message_t p;
+        std::wstring s;
+        s.assign(users[socket].begin(), users[socket].end());
+        s.append(L" : ");
+        s.append(chat->msg);
+        std::copy(s.begin(), s.end(), chat->msg);
+        for (auto& i : users) {
+            yc_net::send(chat, i.first);
+        }
         });
 
-    yc_net::add_sync_worker(w, loop);
+    //yc_net::add_sync_worker(w, loop);
 
     main_server();
 }
