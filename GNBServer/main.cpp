a1a2c8a9d0788732e5536c58306028edf1c6785d
yuchanahn <37448462+yuchanahn@users.noreply.github.com>
2022-03-12 18:08:11 +0900

    마우스 이동에 관한 네트워크 동기화 테스트

--- GNBServer/main.cpp
+++ GNBServer/main.cpp
@@ -6,6 +6,15 @@
 #include <map>
 auto w = yc_net::create_worker();
 
+char number_cnt = 0;
+
+struct user_t {
+    std::string ip_addrass;
+    char character_number = -1;
+    vec3d_t cur_pos;
+};
+
+
 
 void loop()
 {
@@ -28,7 +37,7 @@ int main()
 {
     packet_data_load();
 
-    std::map<yc::socket_t, std::string> users;
+    std::map<yc::socket_t, user_t> users;
 
     server_setting = server_setting_t{
         .io_thread_number = 2,
@@ -38,7 +47,7 @@ int main()
 
     yc_net::connect_callback = [&](yc::socket_t socket) {
         printf("connect! [SOCKET : %lld, ADDR : %s]\n", socket, yc_net::get_clnt_addrs(socket).c_str());
-        users[socket] = yc_net::get_clnt_addrs(socket).c_str();
+        users[socket].ip_addrass = yc_net::get_clnt_addrs(socket).c_str();
     };
 
     yc_net::disconnect_callback = [&](yc::socket_t socket) {
@@ -51,26 +60,43 @@ int main()
     yc_net::bind_ev<p_chat_message_t>([&](p_chat_message_t* chat, yc::socket_t socket) {
         wprintf(std::format(L"SOCKET : {} - {}\n", socket, chat->msg).c_str());
         std::wstring s;
-        s.assign(users[socket].begin(), users[socket].end());
+        s.assign(users[socket].ip_addrass.begin(), users[socket].ip_addrass.end());
         s.append(L" : ");
         s.append(chat->msg);
         std::copy(s.begin(), s.end(), chat->msg);
         for (auto& i : users) {
             yc_net::send(chat, i.first);
         }
+        if (users[socket].character_number == -1) {
+            vec3d_t pos{
+                rand() % 100,
+                rand() % 100,
+                rand() % 100,
+            };
+            yc_net::add_sync_worker(w, [&, socket, pos] {
+                users[socket].character_number = number_cnt++;
+                p_spawn_character_t p{
+                    users[socket].character_number,
+                };
+                for (auto& i : users) {
+                    p.pos = pos;
+                    p.IsMyCharacter = (i.first == socket);
+                    yc_net::send(&p, i.first);
+                }
+                });
+        }
         });
 
     auto send_all_user = [&](yc::socket_t sock) {
         p_chat_message_t p { };
         std::wstring s;
-        s.assign(users[sock].begin(), users[sock].end());
+        s.assign(users[sock].ip_addrass.begin(), users[sock].ip_addrass.end());
         s.append(L"님이 SSR 등급 캐릭터를 뽑았습니다!");
         std::copy(s.begin(), s.end(), p.msg);
         for (auto& i : users) {
             yc_net::send(&p, i.first);
         }
     };
-
 
     yc_net::bind_ev<p_gacha_start_t>([&](p_gacha_start_t* gacha, yc::socket_t socket) {
         
@@ -84,6 +110,28 @@ int main()
 
         yc_net::send(&gr, socket);
 
+        });
+    yc_net::bind_ev<p_mouse_event_t>([&](p_mouse_event_t* mouse_ev, yc::socket_t socket) {
+
+        if (mouse_ev->right) {
+            p_movement_character_t p{
+                users[socket].character_number,
+                mouse_ev->start_pos,
+                mouse_ev->pos
+            };
+            for (auto& i : users) {
+                yc_net::send(&p, i.first);
+            }
+        }
+        else {
+            p_attack_t p{
+                users[socket].character_number,
+                mouse_ev->start_pos
+            };
+            for (auto& i : users) {
+                yc_net::send(&p, i.first);
+            }
+        }
         });
 
 
--- inc/packet/packet_data.hpp
+++ inc/packet/packet_data.hpp
@@ -3,18 +3,53 @@
 
 #pragma pack(push, 1)
 
+struct vec3d_t {
+	float x;
+	float y;
+	float z;
+};
+
 struct p_chat_message_t {
 	wchar_t msg[300];
 };
+
 
 struct p_gacha_start_t {
 	char cnt;
 };
 
+
 struct p_gacha_result_t {
 	char r[10];
 	char size;
 };
+
+
+struct p_mouse_event_t {
+	bool right;
+	vec3d_t start_pos;
+	vec3d_t pos;
+};
+
+struct p_spawn_character_t {
+	char number;
+	bool IsMyCharacter;
+	vec3d_t pos;
+};
+
+struct p_movement_character_t {
+	char number;
+	vec3d_t start_pos;
+	vec3d_t pos;
+};
+
+
+struct p_attack_t {
+	char number;
+	vec3d_t start_pos;
+};
+
+
 
 #pragma pack(pop)
 
@@ -22,4 +57,8 @@ auto packet_data_load = [] {
 	ioev::Map<p_chat_message_t>().To<0>();
 	ioev::Map<p_gacha_start_t>().To<1>();
 	ioev::Map<p_gacha_result_t>().To<2>();
+	ioev::Map<p_mouse_event_t>().To<3>();
+	ioev::Map<p_spawn_character_t>().To<4>();
+	ioev::Map<p_movement_character_t>().To<5>();
+	ioev::Map<p_attack_t>().To<6>();
 };
